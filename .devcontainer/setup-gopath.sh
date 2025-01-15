ce832b170a56beaa8732b2ba3930f54a6b171a30
Lynott, David J <david.lynott@collins.com>
2025-01-15 22:45:38 +0000

    Uh... maybe this will give us more data

--- .devcontainer/setup-gopath.sh
+++ .devcontainer/setup-gopath.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 goPath=$(which go > /dev/null 2>&1)
-if [ $? -ne 0 ]; then
-   # Add go to the path
+if [ "$?" -ne 0 ]; then
+   echo "[INFO] Go is not in the path... adding!"
    newPath="export PATH=\$PATH:/usr/local/go/bin:$HOME/go/bin"
    echo "${newPath}" >> ~/.bashrc
    echo "${newPath}" >> ~/.profile
+else
+   echo "[INFO] Go is in the path already!"
 fi
